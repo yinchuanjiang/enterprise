6856447c43433243eb6653409bdf76ae9e56027f
安小游 <471607346@qq.com>
2020-04-13 11:32:03 +0800

    commit

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -24,18 +24,18 @@
         </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-6 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-yellow">
-            <div class="inner">
-                <h3>{{$userOfGoverment}}</h3>
-                <p>机构用户数量</p>
-            </div>
-            <div class="icon">
+    {{--<div class="col-lg-6 col-xs-6">--}}
+        {{--<!-- small box -->--}}
+        {{--<div class="small-box bg-yellow">--}}
+            {{--<div class="inner">--}}
+                {{--<h3>{{$userOfGoverment}}</h3>--}}
+                {{--<p>机构用户数量</p>--}}
+            {{--</div>--}}
+            {{--<div class="icon">--}}
                 {{--<i class="fa fa-shopping-cart"></i>--}}
-            </div>
-        </div>
-    </div>
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
     <!-- ./col -->
     <div class="col-lg-6 col-xs-6">
         <!-- small box -->
@@ -52,18 +52,18 @@
     </div>
 
     <!-- ./col -->
-    <div class="col-lg-6 col-xs-6">
-        <!-- small box -->
-        <div class="small-box bg-blue">
-            <div class="inner">
-                <h3>{{$knowledgeCount}}</h3>
-                <p>知识产权数量</p>
-            </div>
-            <div class="icon">
+    {{--<div class="col-lg-6 col-xs-6">--}}
+        {{--<!-- small box -->--}}
+        {{--<div class="small-box bg-blue">--}}
+            {{--<div class="inner">--}}
+                {{--<h3>{{$knowledgeCount}}</h3>--}}
+                {{--<p>知识产权数量</p>--}}
+            {{--</div>--}}
+            {{--<div class="icon">--}}
                 {{--<i class="fa fa-shopping-cart"></i>--}}
-            </div>
-        </div>
-    </div>
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
     <!-- ./col -->
     <div class="col-lg-6 col-xs-6">
         <!-- small box -->
